46f38dc3bc436d6181e6f289c707dc2816b2f344
adityax1412 <adityanaidu2004@gmail.com>
2025-07-31 01:10:15 +0530

    test

--- Evaluation/measure_ssim_psnr.py
+++ Evaluation/measure_ssim_psnr.py
@@ -45,21 +45,37 @@ Get datasets from
  - http://irvlab.cs.umn.edu/resources/euvp-dataset
  - http://irvlab.cs.umn.edu/resources/ufo-120-dataset
 """
-gtr_dir = "/home/xahid/datasets/released/EUVP/test_samples/GTr/"
+
+ssims, psnrs = [], []
+
+im_res=(256, 256)
+
+gtr_path = "D:/Downloads/gt_enhanced (1).png"
 #gtr_dir = "/home/xahid/datasets/released/UFO-120/TEST/hr/"
 
 ## generated im paths
-gen_dir = "eval_data/euvp_test/funie-gan/" 
+gen_path = "D:/Downloads/fake_epoch_1999 (4).png" 
 #gen_dir = "eval_data/ufo_test/deep-sesr/" 
 
+r_im = Image.open(gtr_path).resize(im_res)
+g_im = Image.open(gen_path).resize(im_res)
+# get ssim on RGB channels
+ssim = getSSIM(np.array(r_im), np.array(g_im))
+ssims.append(ssim)
+# get psnt on L channel (SOTA norm)
+r_im = r_im.convert("L"); g_im = g_im.convert("L")
+psnr = getPSNR(np.array(r_im), np.array(g_im))
+psnrs.append(psnr)
 
+print("ssim",ssims)
+print("psnr",psnrs)
 ### compute SSIM and PSNR
-SSIM_measures, PSNR_measures = SSIMs_PSNRs(gtr_dir, gen_dir)
+"""SSIM_measures, PSNR_measures = SSIMs_PSNRs(gtr_dir, gen_dir)
 print ("SSIM on {0} samples".format(len(SSIM_measures)))
 print ("Mean: {0} std: {1}".format(np.mean(SSIM_measures), np.std(SSIM_measures)))
 
 print ("PSNR on {0} samples".format(len(PSNR_measures)))
-print ("Mean: {0} std: {1}".format(np.mean(PSNR_measures), np.std(PSNR_measures)))
+print ("Mean: {0} std: {1}".format(np.mean(PSNR_measures), np.std(PSNR_measures)))"""
 
 
 
